339362c20bfb4c6668bd74308640443b405d6097
byronbiney <bbiney1@swarthmore.edu>
2017-08-11 15:08:48 +0000

    if corpus_rank > 4000 display 'rare' feature added to datatables api in javascript

--- static/main.js
+++ static/main.js
@@ -1470,13 +1470,14 @@ function initTable() {
                 columns.push({
                   "name": $(this).data("fieldname"),
                   "data": "fields."+$(this).data("fieldname"),
-                  /*"render": function( data, type, full, meta) {
-                      if (data > 4000) {
-                        return "rare";
-                      } else {
-                        return data;
-                     }
-                   },*/
+                  "render": function( data, type, full, meta) {
+                      if (type == 'display') {
+                           if (data > 4000) {
+                               return "rare";
+                           }
+                        }
+                      return data
+                     },
                   "visible": $(this).data("visible"),
              });
            } else if ($(this).data("fieldname") == "english_extended") {
